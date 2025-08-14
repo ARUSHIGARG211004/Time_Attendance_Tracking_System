6b3c434a199421089826773f8b09d6c5d2392fee
ARUSHIGARG211004 <121043298+ARUSHIGARG211004@users.noreply.github.com>
2025-08-14 12:05:47 +0530

    completed

---
+++ backend/models/Attendance.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const AttendanceSchema = new mongoose.Schema({
+  employeeId: String,
+  name: String,
+  status: String, // 'present' or 'absent'
+  time: String,
+  date: String,
+  day: String,
+});
+
+module.exports = mongoose.model("Attendance", AttendanceSchema);
+
+// const mongoose = require("mongoose");
+
+// const AttendanceSchema = new mongoose.Schema({
+//   employeeId: String,
+//   name: String,
+//   status: String, // 'present' or 'absent'
+//   time: String,
+//   date: String,
+//   day: String,
+// });
+
+// module.exports = mongoose.model("Attendance", AttendanceSchema);
